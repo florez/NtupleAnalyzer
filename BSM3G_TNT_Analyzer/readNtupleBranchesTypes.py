2f4b9e8c7b6d7853d7ee0cc711165b2cf9cb847d
Denis Rathjens <Denis.Rathjens@cern.ch>
2015-04-10 22:03:04 +0200

    Added command line options, replaced tree.Draw with tree.MakeCode and merged steps needed to generate the analysis skeleton

--- BSM3G_TNT_Analyzer/readNtupleBranchesTypes.py
+++ BSM3G_TNT_Analyzer/readNtupleBranchesTypes.py
@@ -1,18 +1,44 @@
 ##############################################################
 # Author: Andres Florez, Universidad de los Andes, Colombia. #
+# Author: Denis Rathjens, University of Hamburg, Germany     #
 ##############################################################
 
-from collections import deque
+from optparse import OptionParser
 
-log_h = open("BSM3G_TNT_Analyzer.h", "w")
+#Parser for command line arguments
+parser = OptionParser()
+parser.add_option("--inFile", dest="inFile", default="OutTree.root", help="filepath for input ntuple file, default=%default")
+parser.add_option("--AnaName",dest="AnaName",default="BSM3G_TNT_Analyzer", help="name of the automatically generated analyzer, default=%default")
+parser.add_option("--dir", dest="dir", default="TNT", help="ntuple path to the tree, default=%default")
+parser.add_option("--tree", dest="tree", default="BOOM", help="ntuple tree name, default=%default")
+(options, args) = parser.parse_args()
+
+print "Input file: " + options.inFile
+
+print "producing list of ntuple branches"
+
+import os, sys
+
+#os.system("python getBrancheslist.py --inFile "+options.inFile+" --dir "+options.dir+" --tree "+options.tree)
+from ROOT import gROOT, TF1, TFile, TTree
+import sys
+
+gROOT.Reset()
+f1 = TFile(options.inFile)
+f1.cd(options.dir)
+tree = f1.Get(options.dir+"/"+options.tree)
+tree.MakeCode(options.inFile.rstrip(".root")+".C")
+
+log_h = open(options.AnaName+".h", "w")
 
 print >> log_h, "////////////////////////////////////////////////////////////////////"
 print >> log_h, "// Developer: Andres Florez, Universidad de los Andes, Colombia. //"
+print >> log_h, "// Developer: Denis Rathjens, University of Hamburg, Germany     //"
 print >> log_h, "//////////////////////////////////////////////////////////////////"
 print >> log_h, ""
 print >> log_h, ""
-print >> log_h, "#ifndef BSM3G_TNT_Analyzer_h" 
-print >> log_h, "#define BSM3G_TNT_Analyzer_h"
+print >> log_h, "#ifndef "+options.AnaName+"_h" 
+print >> log_h, "#define "+options.AnaName+"_h"
 print >> log_h, ""
 print >> log_h, "#include <iostream>"
 print >> log_h, "#include <fstream>"
@@ -31,97 +57,85 @@ print >> log_h, "#include <TTree.h>"
 print >> log_h, ""
 print >> log_h, "using namespace std;"
 print >> log_h, ""
-print >> log_h, "class BSM3G_TNT_Analyzer {"
+print >> log_h, "class "+options.AnaName+" {"
 print >> log_h, "public :"
-print >> log_h, "   BSM3G_TNT_Analyzer();"
-print >> log_h, "   ~BSM3G_TNT_Analyzer();"
+print >> log_h, "   "+options.AnaName+"();"
+print >> log_h, "   ~"+options.AnaName+"();"
 print >> log_h, ""
-print >> log_h, "   void setBranchAddress(TTree* Tree);"
+print >> log_h, "   void setBranchAddress(TTree* "+options.tree+");"
 
-queue   = deque()
+ReadLeaves=0
 
-with open("outputBranchFile") as f:
-    for line in f:
-        for word in line.split():
-           word = word.replace(":", "")
-           queue.append(word)
+with open(options.inFile.rstrip(".root")+".C") as f:
+    for line in f:	
+    	if ReadLeaves==1:
+		if line.find("// Set branch addresses.")!=-1:
+			print >> log_h, "};"
+			print >> log_h, "#endif"
+			break
+		print >> log_h, line.rstrip("\n")
+    	if line.find("//Declaration of leaves types")!=-1:
+		ReadLeaves=1
+    
+print "writing "+options.AnaName+".cc"
 
+log_cc = open(options.AnaName+".cc", "w")
 
-index = 3
-
-for x in range(len(queue)):
-    if x == index:
-        getVector =  queue[index+2]
-
-        if getVector.find('vector') != -1:
-             print >> log_h, "  ", queue[index+2],"*", queue[index],";"
-        if getVector.find('/D') != -1:
-             print >> log_h, "   double ", queue[index],";" 
-        if getVector.find('/F') != -1:
-             print >> log_h, "   float ", queue[index],";"
-        if getVector.find('/I') != -1:
-             print >> log_h, "   int ", queue[index],";"
-        
-        index+=31
-
-print >> log_h, ""
-
-index_b = 3
-
-print >> log_h, "};"
-print >> log_h, "#endif"
-
-log_cc = open("BSM3G_TNT_Analyzer.cc", "w")
-
-print >> log_cc, "#include \"BSM3G_TNT_Analyzer.h\""
+print >> log_cc, "#include \""+options.AnaName+".h\""
 print >> log_cc, ""
 print >> log_cc, "int main (int argc, char *argv[])"
 print >> log_cc, "{"
 print >> log_cc, ""
 print >> log_cc, "  TApplication app(\"App\",&argc, argv);"
 print >> log_cc, "  gROOT->ProcessLine(\"#include <vector>\");"
-print >> log_cc, "  BSM3G_TNT_Analyzer BSM3G_TNT_analyzer;"
+print >> log_cc, "  "+options.AnaName+" "+options.AnaName+";"
 print >> log_cc, ""
 print >> log_cc, "}"
-print >> log_cc, "BSM3G_TNT_Analyzer::BSM3G_TNT_Analyzer()"
+print >> log_cc, ""+options.AnaName+"::"+options.AnaName+"()"
 print >> log_cc, "{"
 print >> log_cc, ""
 print >> log_cc, "  TFile *f = new TFile (\"OutTree.root\");"
 print >> log_cc, "  f->cd(\"TNT\");"
-print >> log_cc, "  TTree* tree = (TTree*)f->Get(\"TNT/BOOM\");"
+print >> log_cc, "  TTree* "+options.tree+" = (TTree*)f->Get(\""+options.dir+"/"+options.tree+"\");"
 print >> log_cc, ""
-print >> log_cc, "  int nentries = (int) tree->GetEntries();"
-print >> log_cc, "  setBranchAddress(tree);"
+print >> log_cc, "  int nentries = (int) "+options.tree+"->GetEntries();"
+print >> log_cc, "  setBranchAddress("+options.tree+");"
 print >> log_cc, ""  
 print >> log_cc, "  for (int i = 0; i < nentries; ++i)"  
 print >> log_cc, "    {"
-print >> log_cc, "       tree->GetEntry(i);"
-print >> log_cc, "       for (int j = 0; j < Muon_pt->size(); j++)"
+print >> log_cc, "       "+options.tree+"->GetEntry(i);"
+print >> log_cc, "       for (int j = 0; j < Muon_pt.size(); j++)"
 print >> log_cc, "         {"
-print >> log_cc, "            cout <<Muon_pt->at(j)<<endl;"
+print >> log_cc, "            cout <<Muon_pt.at(j)<<endl;"
 print >> log_cc, "         }"
 print >> log_cc, "     }"
 print >> log_cc, ""
 print >> log_cc, "}"
 print >> log_cc, ""
-print >> log_cc, "BSM3G_TNT_Analyzer::~BSM3G_TNT_Analyzer()"
+print >> log_cc, ""+options.AnaName+"::~"+options.AnaName+"()"
 print >> log_cc, "{"
 print >> log_cc, ""
 print >> log_cc, "  // do anything here that needs to be done at desctruction time"
 print >> log_cc, ""
 print >> log_cc, "}"
 print >> log_cc, ""
-print >> log_cc, "void BSM3G_TNT_Analyzer::setBranchAddress(TTree* Tree)"
+print >> log_cc, "void "+options.AnaName+"::setBranchAddress(TTree* "+options.tree+")"
 print >> log_cc, "{"
 print >> log_cc, ""
 
-index = 3
+ReadBranches=0
 
-for x in range(len(queue)):
-    if x == index:
-        setbranches = "  Tree->SetBranchAddress(\""+queue[index]+"\", &"+queue[index]+");"
-        print >> log_cc, setbranches 
-        index+=31
-
-print >> log_cc, "}"
+with open(options.inFile.rstrip(".root")+".C") as f:
+	for line in f:	
+    		if ReadBranches==1:
+			if line.find("//     This is the loop skeleton")!=-1:
+				print >> log_cc, "};"
+				break
+			print >> log_cc, line.rstrip("\n")				
+    		if line.find("// Set branch addresses.")!=-1:
+			ReadBranches=1
+		
+os.system("rm "+options.inFile.rstrip(".root")+".C")		
+		
+print "All done! Run: \"make "+options.AnaName+" to compile the analyzer skeleton"		
 
